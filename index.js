73a8b62a97972dbbd91e79b893068c5fe71f3d30
rpwv <77336459+rpwv@users.noreply.github.com>
2021-02-26 18:16:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = 'https://rpwzu.herokuapp.com'
 const bodyParser = require('body-parser')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
